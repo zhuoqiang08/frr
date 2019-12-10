7668cb9605d779d85c86e0e991fd598b464209b6
GalaxyGorilla <sascha@netdef.org>
2019-12-10 10:33:15 +0000

    Fix removal of SR Policies (zebra wasn't cleaned up)

--- pathd/path_nb_config.c
+++ pathd/path_nb_config.c
@@ -255,6 +255,8 @@ int pathd_te_sr_policy_candidate_path_destroy(enum nb_event event,
 	te_candidate_path = nb_running_get_entry(dnode, NULL, true);
 	te_sr_policy_candidate_path_delete(te_candidate_path);
 
+	te_sr_policy_candidate_path_set_active(te_candidate_path->sr_policy);
+
 	return NB_OK;
 }
 
